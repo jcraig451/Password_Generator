f9e3a6c69d2026f03b48d6f92407f4de15235dab
Josh Craig <jtcraig451@gmail.com>
2019-11-02 00:15:55 -0400

    Created index.html file with style.css and main.js file to create a passw ord generator

---
+++ main.js
@@ -0,0 +1,102 @@
+
+
+
+var resultEl = document.getElementById('result');
+var generateEl = document.getElementById('generate');
+var clipboardEl = document. getElementById('clipboard');
+
+
+
+generateEl.addEventListener('click', function(){
+    var lengthEl = prompt("Select length of password between 8-120 characters:", "8-120");
+    if (lengthEl == null || lengthEl == "" || lengthEl < 8 || lengthEl > 120){
+        alert("Not a valid entry try again")
+        return ""
+    }
+
+    var lowerEl = confirm("Do you want your password to contain a lowercase letter?");
+    var upperEl = confirm("Do you want your password to contain an UPPERCASE letter?");
+    var numberEl = confirm("Do you want your password to contain a number?");
+    var symbolEl = confirm("Do you want your password to contain a symbol?");
+
+    if (lowerEl + upperEl + numberEl + symbolEl == false){
+        alert("Not a valid entry try again")
+    }
+    
+    var length = lengthEl.value;
+    
+    resultEl.innerHTML = generatePW(lowerEl, upperEl, numberEl, symbolEl, lengthEl);  
+    
+    clipboardEl.addEventListener('click', function(){
+        var copyText = document.getElementById("result");
+        copyText.select();
+        // copyText.setSelectionRange(0,99999);
+    
+        document.execCommand('copy');
+        alert("Copied password to clipboard: " + copyText.value);
+        // var resultText = document.createElement('resultText');
+        // var copyPW = resultEl.innerHTML;
+    
+        // if(!copPW){
+        //     return "";
+        // }
+    
+        // textarea.value = copyPW;
+        // document.body.appendChild(textarea);
+        // textarea.select();
+        // document.execCommand('copy');
+        // textarea.remove();
+        // alert("Password copied to clipboard:")
+    
+    });
+    
+})
+
+function generatePW(lower, upper, number, symbol, length){
+    var userGeneratedPW = '';
+    var variablesCount = lower + upper + number + symbol;
+    var variablesArry = [{lower}, {upper}, {number}, {symbol}].filter(item => Object.values(item)[0]);
+
+    if(variablesCount == 0){
+        return "";
+    }
+
+    for(var b = 0; b < length; b += variablesCount){
+        variablesArry.forEach(type => {
+            var funcName = Object.keys(type)[0];
+
+            userGeneratedPW += randomFunc[funcName]();
+
+        });
+    }
+
+    var finalPW = userGeneratedPW.slice(0, length);
+    return finalPW
+
+}
+
+var randomFunc = {
+    lower: randomLwCase,
+    upper: randomUpCase,
+    number: randomNum,
+    symbol: randomSym,
+}
+
+//Random value Generator functions using the Character Set values 
+//Uppercase function
+function randomUpCase(){
+    return String.fromCharCode(Math.floor(Math.random()*26) +65);
+}
+//Lowercase fuction
+function randomLwCase(){
+    return String.fromCharCode(Math.floor(Math.random()*26) +97);
+}
+//Number fuction
+function randomNum(){
+    return String.fromCharCode(Math.floor(Math.random()*10) +48);
+}
+//Symbol Function set as an array for ease of coding from Character Set
+function randomSym(){
+    var symbols = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
+    return symbols[Math.floor(Math.random() *symbols.length)];
+}
